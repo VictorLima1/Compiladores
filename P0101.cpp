248989729c3735908271bbb0f715cf5a72518588
Victor <37022914+VictorLima1@users.noreply.github.com>
2019-09-01 16:26:32 -0300

    Add files via upload

--- P0101.cpp
+++ P0101.cpp
@@ -7,26 +7,7 @@
 // Guilherme Ferreira - 22.216.024-4
 
 void criaGoto(char* nomePrograma){
-	strcat(nomePrograma,".cpp");
-	FILE *fp = fopen(nomePrograma, "w+");
 	
-	fprintf(fp, "#include <stdio.h> \n" \
-				"#include <stdlib.h> \n" \
-				"#include <string.h> \n" \
-				"#include <conio.h>\n\n");
-				
-				
-	fprintf(fp, "int main() {\n");
-	
-	fprintf(fp,"\tchar f[200];\n\tint p = 0;\n\tprintf(\"Cadeia: \");\n\tfgets(f, 200, stdin);\n\tgoto e0;\n\n");
-	
-	
-	
-	
-	fprintf(fp,"\n\treturn 0;");
-	fprintf(fp,"\n}");
-	
-	fclose(fp);
 }
 
 //void criaFuncao(char* nomePrograma, int estados, int simbolos, char* sigma, int delta[estados][simbolos], int inicial){
@@ -85,7 +66,7 @@ int main(void) {
 	scanf("%s",&nomePrograma);
 	
 	if(opcao == 1){
-		int i, j;
+		int i, j, a = 0;
         //criaFuncao(nomePrograma, estados, simbolos, sigma, delta, inicial);
         strcat(nomePrograma,".cpp");
 		FILE *fp = fopen(nomePrograma, "w+");
@@ -116,8 +97,9 @@ int main(void) {
 
 			for(int k = 0; k < finais; k++){
                 if(i == final[k]){
-					if(delta[i][j] == -1 && delta[i][j+1] == -1){
-                        fprintf(fp, "\n\t\tif(f[p] == '\\0') {\n\t\t\taceita();\n\t}");
+					if(i == finais){
+                        fprintf(fp, "\n\tif(f[p] == '\\0') {\n\t\taceita();\n\t}");
+                        a = 1;
 					}
 					else{
                          fprintf(fp, "\n\telse\n\t\tif(f[p] == '\\0') {\n\t\t\taceita();\n\t}");
@@ -125,19 +107,75 @@ int main(void) {
                    
 				}
 			}
-			fprintf(fp, "\n\telse{\n\t\trejeita();\n\t}");
+			
+			
+			fprintf(fp, "\n\telse {\n\t\trejeita();\n\t}");
 			fprintf(fp, "\n}\n");
 		}
 
-		fprintf(fp, "\nvoid aceita() {\n\tputs(\"Aceita\");\n\texit(0);\n}");
-		fprintf(fp, "\nvoid rejeita() {\n\tputs(\"Rejeita\");\n\texit(0);\n}");
+		fprintf(fp, "\nvoid aceita() {\n\tputs(\"Aceita\");\n\tgetch();\n\texit(0);\n}\n");
+		fprintf(fp, "\nvoid rejeita() {\n\tputs(\"Rejeita\");\n\tgetch();\n\texit(0);\n}\n");
 
-		fprintf(fp, "\nint main() {\n\tprintf(\"Digite a palavra: \");\n\tgets(f);\n\te%d();\n\treturn 0;\n}", inicial);
+		fprintf(fp, "\nint main() {\n\tprintf(\"Digite a palavra: \");\n\tgets(f);\n\te%d();\n\tgetch();\n\treturn 0;\n}", inicial);
 
 		fclose(fp);
 	}
 	else if(opcao == 2){
-		criaGoto(nomePrograma);
+		int i, j, a;
+		strcat(nomePrograma,".cpp");
+		FILE *fp = fopen(nomePrograma, "w+");
+		
+		fprintf(fp, "#include <stdio.h> \n" \
+					"#include <stdlib.h> \n" \
+					"#include <string.h> \n" \
+					"#include <conio.h>\n" \
+					"#include <iostream> \n\nusing namespace std;\n\n");
+					
+					
+		fprintf(fp, "int main() {\n");
+		
+		fprintf(fp,"\tchar f[200];\n\tint p = 0;\n\tprintf(\"Cadeia: \");\n\tcin>>f;\n\tgoto e%d;\n\n", inicial);
+		
+		for(i = 0; i < estados; i++){
+			fprintf(fp, "\n\te%d:", i);
+			for(j = 0; j < simbolos; j++){
+                if(delta[i][j] != -1){
+                    fprintf(fp, "\n\t\tif(f[p] == '%c') {\n", sigma[j]);
+					fprintf(fp, "\t\t\tp++;\n\t\t\tgoto e%d;\n\t\t}\n", delta[i][j]);
+				}
+			}
+			
+			for(int k = 0; k < finais; k++){
+                if(i == final[k]){
+					if(i == finais){
+						printf("%d",final[i]);
+                        fprintf(fp, "\n\t\tif(f[p] == '\\0') {\n\t\t\tgoto aceita;\n\t\t}");
+                        a = 1;
+					}
+					else{
+						printf("%d",final[i]);
+                         fprintf(fp, "\n\t\telse\n\t\t\tif(f[p] == '\\0') {\n\t\t\t\tgoto aceita;\n\t\t\t}\n");
+					}
+                   
+				}
+			}
+			
+			if(a != 1){
+				fprintf(fp, "\n\t\telse {\n\t\t\tgoto rejeita;\n\t\t}\n");	
+			}
+		}
+		
+		
+		if(a == 1){
+				fprintf(fp, "\n\t\telse{\n\t\t\tgoto rejeita;\n\t\t}\n");
+			}
+			
+		fprintf(fp, "\n\taceita: \n\t\tputs(\"Aceita\");\n\t\texit(0);\n");
+		fprintf(fp, "\n\trejeita: \n\t\tputs(\"Rejeita\");\n\t\texit(0);\n");
+
+		fprintf(fp,"\n}");
+		
+		fclose(fp);
 	}
 	
 	printf("\n");
